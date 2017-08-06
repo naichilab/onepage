2cdc6375a23d70e2f6e74f296638736d0797c9fe
Subarunari <koyadouhu@gmail.com>
2017-08-06 21:37:33 +0900

    change jinja2 delimiter for Vue.js

--- onepage/__init__.py
+++ onepage/__init__.py
@@ -4,6 +4,18 @@ from onepage.views import login, signup, novel
 
 
 app = Flask(__name__)
+
+jinja_options = app.jinja_options.copy()
+jinja_options.update(dict(
+    block_start_string='<%',
+    block_end_string='%>',
+    variable_start_string='<<',
+    variable_end_string='>>',
+    comment_start_string='<#',
+    comment_end_string='#>'
+))
+app.jinja_options = jinja_options
+
 app.register_blueprint(login.app)
 app.register_blueprint(signup.app)
 app.register_blueprint(novel.app)
