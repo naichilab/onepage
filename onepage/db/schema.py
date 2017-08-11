a09d2c155a40c7a63f10f2d00009adee73e471f1
Subarunari <koyadouhu@gmail.com>
2017-08-11 22:54:22 +0900

    add schema and model for novel

--- onepage/db/schema.py
+++ onepage/db/schema.py
@@ -4,14 +4,37 @@ from orator import Schema
 from onepage.db import dbconfig
 
 
+USER_TABLE_NAME = 'Users'
+NOVEL_TABLE_NAME = 'Novels'
+CATEGORY_TABLE_NAME = 'Categories'
+
 db = DatabaseManager(dbconfig.DATABASES)
 schema = Schema(db)
 
-with schema.create('Users') as table:
-    table.increments('id')
-    table.string('email')
-    table.string('password_hash')
-    table.string('pen_name')
-    table.timestamps()
+if not schema.has_table(USER_TABLE_NAME):
+    with schema.create(USER_TABLE_NAME) as table:
+        table.increments('id')
+        table.string('email')
+        table.string('password_hash')
+        table.string('pen_name')
+        table.timestamps()
 
-    table.unique('email')
+        table.unique('email')
+
+if not schema.has_table(CATEGORY_TABLE_NAME):
+    with schema.create(CATEGORY_TABLE_NAME) as table:
+        table.increments('id')
+        table.string('name')
+
+if not schema.has_table(NOVEL_TABLE_NAME):
+    with schema.create(NOVEL_TABLE_NAME) as table:
+        table.increments('id')
+        table.string('title')
+        table.string('text')
+        table.timestamps()
+        table.big_integer('user_id')
+        table.drop_foreign('Novels_user_id_foreign')
+        table.foreign('user_id').references('id').on(USER_TABLE_NAME)
+        table.big_integer('category_id')
+        table.drop_foreign('Novels_category_id_foreign')
+        table.foreign('category_id').references('id').on(CATEGORY_TABLE_NAME)
--- onepage/models.py
+++ onepage/models.py
@@ -1,15 +1,16 @@
-from orator import DatabaseManager, Model
-#from orator.orm import scope
+from orator import DatabaseManager
+from orator import Model
+from orator.orm import has_many
+from orator.orm import belongs_to
 
 from onepage.db import dbconfig
+from onepage.db import schema
 
-
-db = DatabaseManager(dbconfig.DATABASES)
-Model.set_connection_resolver(db)
+Model.set_connection_resolver(DatabaseManager(dbconfig.DATABASES))
 
 
 class User(Model):
-    __table__ = 'Users'
+    __table__ = schema.USER_TABLE_NAME
     __fillable__ = ['pen_name']
     __guarded__ = ['email', 'password_hash']
     __timestamps__ = False
@@ -17,3 +18,29 @@ class User(Model):
     @classmethod
     def find_by_email(cls, email):
         return cls.query().where_email(email).get().first()
+
+    @has_many
+    def novels(self):
+        return Novel
+
+
+class Category(Model):
+    __table__ = schema.CATEGORY_TABLE_NAME
+    __fillable__ = ['name']
+
+    @has_many
+    def novels(self):
+        return Novel
+
+
+class Novel(Model):
+    __table__ = schema.NOVEL_TABLE_NAME
+    __fillable__ = ['title', 'text', 'user_id', 'category_id']
+
+    @belongs_to
+    def user(self):
+        return User
+
+    @belongs_to
+    def category(self):
+        return Category
